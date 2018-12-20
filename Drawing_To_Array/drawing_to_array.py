a638c905a184927781a3d6b968bb7157f63a432f
Grebtsew <danwe980@student.liu.se>
2018-12-20 01:19:45 +0100

    fixed and cleaned implementation

--- Drawing_To_Array/drawing_to_array.py
+++ Drawing_To_Array/drawing_to_array.py
@@ -1,173 +1,212 @@
 
 import cv2
 import numpy as np
-from matplotlib import pyplot as plt
 
 '''
-https://docs.opencv.org/3.1.0/d3/db4/tutorial_py_watershed.html
+Main function for testing
 
 https://mathematica.stackexchange.com/questions/19546/image-processing-floor-plan-detecting-rooms-borders-area-and-room-names-t
-
-Using opencv
 '''
+def main():
+    img = cv2.imread("example3.png")
 
-img = cv2.imread("example.png")
+    height, width, channels = img.shape
+    blank_image = np.zeros((height,width,3), np.uint8) # output image same size as original
 
-gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    # grayscale
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
-ret, thresh = cv2.threshold(gray,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+    # create wall image
+    wall_img = wall_filter(gray)
 
-# noise removal
-kernel = np.ones((3,3),np.uint8)
-opening = cv2.morphologyEx(thresh,cv2.MORPH_OPEN,kernel, iterations = 2)
+    # detect corners
+    corners, img = detectCorners(wall_img, img, [255,0,0])
 
-sure_bg = cv2.dilate(opening,kernel,iterations=3)
+    # detect boxes
+    boxes, img = detectPreciseBoxes(wall_img, img ,[100,100,0])
 
-dist_transform = cv2.distanceTransform(opening,cv2.DIST_L2,5)
-ret, sure_fg = cv2.threshold(dist_transform,0.02*dist_transform.max(),255,0)
+    #detect outer Contours
+    contour, img = detectOuterContours(gray, img, [0,255,0])
 
-sure_fg = np.uint8(sure_fg)
-unknown = cv2.subtract(sure_bg,sure_fg)
+    # Show images
+    cv2.imshow("a", img)
+    #cv2.imshow("b", wall_img)
+    #cv2.imshow("c", gray)
+
+    # Exit
+    cv2.waitKey()
+    cv2.destroyAllWindows()
 
 
+'''
+Filter walls
+@Return our thick wallmap
+'''
+def wall_filter(gray):
+    ret, thresh = cv2.threshold(gray,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+
+    # noise removal
+    kernel = np.ones((3,3),np.uint8)
+    opening = cv2.morphologyEx(thresh,cv2.MORPH_OPEN,kernel, iterations = 2)
+
+    sure_bg = cv2.dilate(opening,kernel,iterations=3)
+
+    dist_transform = cv2.distanceTransform(opening,cv2.DIST_L2,5)
+    ret, sure_fg = cv2.threshold(0.5*dist_transform,0.2*dist_transform.max(),255,0)
+
+    sure_fg = np.uint8(sure_fg)
+    unknown = cv2.subtract(sure_bg,sure_fg)
+
+    return unknown
 
 '''
 Find each corner
+'''
+def detectCorners(detect_img, output_img = None, color = [255,0,0] ):
+    corners = cv2.goodFeaturesToTrack(detect_img, 1000, 0.1, 1)
+    corners = np.int0(corners)
+
+    if output_img is not None:
+        for corner in corners:
+            x,y = corner.ravel()
+            cv2.circle(output_img,(x,y),3,255,-1)
+    return corners, output_img
 
 '''
-corners = cv2.goodFeaturesToTrack(unknown, 100, 0.01, 10)
-corners = np.int0(corners)
+Watershed
 
+https://docs.opencv.org/3.1.0/d3/db4/tutorial_py_watershed.html
+'''
+def Watermark(detect_img, output_img = None, color = [255,0,0]):
+    ret, markers = cv2.connectedComponents(detect_img)
 
-for corner in corners:
-    x,y = corner.ravel()
-    cv2.circle(unknown,(x,y),3,255,-1)
-
+    markers = markers+1
+    markers[unknown==255] = 0
+    if output_img is not None:
+        markers = cv2.watershed(output_img,markers)
+        output_img[markers == -1] = color
+    return markers
 
 '''
-Show on original image
-ret, markers = cv2.connectedComponents(sure_fg)
-
-markers = markers+1
-markers[unknown==255] = 0
-markers = cv2.watershed(img,markers)
-img[markers == -1] = [255,0,0]
-
-
-cv2.imshow('img', img)
-'''
-
-
-'''
-
 Get center of objects
+
 https://docs.opencv.org/3.0-beta/doc/py_tutorials/py_feature2d/py_features_harris/py_features_harris.html
-
-# find centroids
-ret, labels, stats, centroids = cv2.connectedComponentsWithStats(sure_fg,4)
-
-
-# define the criteria to stop and refine the corners
-criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.001)
-corners = cv2.cornerSubPix(unknown,np.float32(centroids),(5,5),(-1,-1),criteria)
-
-# Now draw them
-res = np.hstack((centroids,corners))
-res = np.int0(res)
-#img[res[:,1],res[:,0]]=[0,0,255]
-#img[res[:,3],res[:,2]] = [0,255,0]
-
-#cv2.imshow('subpixel',img)
 '''
+def detectCenterBoxes(detect_img, output_img = None, color = [100,100,0]):
+    # find centroids
+    ret, labels, stats, centroids = cv2.connectedComponentsWithStats(detect_img,4)
 
+    # define the criteria to stop and refine the corners
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.001)
+    corners = cv2.cornerSubPix(detect_img,np.float32(centroids),(5,5),(-1,-1),criteria)
 
-'''
-corners = cv2.cornerHarris(unknown,2,3,0.04)
-res = cv2.dilate(corners, None, iterations=3)
-'''
-'''
-res = stats
-i = len(res)-1
-print(res)
-while i > 0:
-    cv2.rectangle(unknown,(int(res[i][0]),int(res[i][1])),(int(res[i][2]),int(res[i][3])),(100,100,0),3)
+    # Now draw them
+    res = np.hstack((centroids,corners))
+    res = np.int0(res)
+    if output_img is not None:
+        output_img[res[:,1],res[:,0]]=[0,0,255]
+        output_img[res[:,3],res[:,2]] = [0,255,0]
 
+        i = len(res)-1
+        print(res)
+        while i >= 0:
+            cv2.rectangle(output_img,(int(res[i][0]),int(res[i][1])),(int(res[i][2]),int(res[i][3])),color,3)
+            i -= 1
+    return res, output_img
 
-    i -= 1
 
 '''
+Bad boxes in image
+@Return boxes
+'''
+def detectUnpreciseBoxes(detect_img, output_img = None, color = [100,100,0]):
+    corners = cv2.cornerHarris(detect_img,2,3,0.04)
+    res = cv2.dilate(corners, None, iterations=3)
 
+    res = stats
+    if output_img is not None:
+        i = len(res)-1
+        while i >= 0:
+            cv2.rectangle(output_img,(int(res[i][0]),int(res[i][1])),(int(res[i][2]),int(res[i][3])),color,3)
+            i -= 1
+    return res, output_img
 
 '''
 Boxes in image
+@Return list of boxes
+
+Source
 https://stackoverflow.com/questions/50930033/drawing-lines-and-distance-to-them-on-image-opencv-python
 '''
-im, contours, hierarchy = cv2.findContours(unknown,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
-area = sorted(contours, key=cv2.contourArea, reverse=True)
+def detectPreciseBoxes(detect_img, output_img = None, color = [100,100,0]):
+    res = []
+
+    im, contours, hierarchy = cv2.findContours(detect_img,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+    #area = sorted(contours, key=cv2.contourArea, reverse=True)
+
+    largest_contour_area = 0
+    for cnt in contours:
+        largest_contour_area = cv2.contourArea(cnt)
+        largest_contour = cnt
+
+        epsilon = 0.001*cv2.arcLength(largest_contour,True)
+        approx = cv2.approxPolyDP(largest_contour,epsilon,True)
+        if output_img is not None:
+            final = cv2.drawContours(output_img, [approx], 0, [100, 200, 0])
+        res.append(approx)
+
+    return res, output_img
 
 '''
-i = len(area)-1
+Remove contours
+@Return list of boxes
 
-while i >= 0:
-    c = area[i]
-    rect = cv2.minAreaRect(c)
-    box = cv2.boxPoints(rect)
-
-    box = np.int0(box)
-    cv2.drawContours(unknown,[box],0,(100,100,0),2)
-    i-= 1
-
-
-
-#cv2.imshow('Corner',unknown)
-#cv2.imshow('image',img)
-#print(len(res))
-#cv2.imshow('show', corners)
+Source
+https://stackoverflow.com/questions/50930033/drawing-lines-and-distance-to-them-on-image-opencv-python
 '''
+def detectAndRemovePreciseBoxes(detect_img, output_img = None, color = [255, 255, 255]):
+    res = []
 
-'''
-Create array of walls
-And show
-'''
+    im, contours, hierarchy = cv2.findContours(detect_img,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+    #area = sorted(contours, key=cv2.contourArea, reverse=True)
 
+    largest_contour_area = 0
+    for cnt in contours:
+        largest_contour_area = cv2.contourArea(cnt)
+        largest_contour = cnt
 
+        epsilon = 0.001*cv2.arcLength(largest_contour,True)
+        approx = cv2.approxPolyDP(largest_contour,epsilon,True)
+        if output_img is not None:
+            cv2.drawContours( output_img,  [approx], -1, color, -1);
+        res.append(approx)
 
-res = []
-box_i = len(area)-1
-corners_i = len(corners)-1
-
-# find box
-
-# find point1 in box
-# find point2 in box
-# draw between them
-
+    return res, output_img
 
 '''
-Draw lines
-https://docs.opencv.org/3.0-beta/doc/py_tutorials/py_imgproc/py_houghlines/py_houghlines.html
+Get the outer side of image
+@Return box
+
+Source
+https://stackoverflow.com/questions/50930033/drawing-lines-and-distance-to-them-on-image-opencv-python
 '''
-'''
-edges = cv2.Canny(unknown,50,150,apertureSize = 3)
-minLineLength = 100
-maxLineGap = 10
-lines = cv2.HoughLinesP(edges,1,np.pi/180,100,minLineLength,maxLineGap)
-i = len(lines)-1
-while i > 0:
-    i -= 1;
-    for x1,y1,x2,y2 in lines[i]:
-        cv2.line(unknown,(x1,y1),(x2,y2),(100,100,0),2)
+def detectOuterContours(detect_img, output_img = None, color = [255, 255, 255]):
 
-#cv2.imshow('houghlines5',img)
+    ret, thresh = cv2.threshold(detect_img, 230, 255, cv2.THRESH_BINARY_INV)
 
-#cv2.line(img,(0,0),(511,511),(255,0,0),5)
-'''
+    img_, contours, hierarchy = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
+    largest_contour_area = 0
+    for cnt in contours:
+        if (cv2.contourArea(cnt) > largest_contour_area):
+            largest_contour_area = cv2.contourArea(cnt)
+            largest_contour = cnt
 
-cv2.imshow('image',unknown)
-cv2.waitKey(0)
-
-
+    epsilon = 0.001*cv2.arcLength(largest_contour,True)
+    approx = cv2.approxPolyDP(largest_contour,epsilon,True)
+    if output_img is not None:
+        final = cv2.drawContours(output_img, [approx], 0, color)
+    return approx, output_img
 
 '''
 Wall exist
@@ -182,5 +221,30 @@ Source:
 https://stackoverflow.com/questions/33065834/how-to-detect-if-a-point-is-contained-within-a-bounding-rect-opecv-python
 '''
 def rectContains(rect,pt):
-    logic = rect[0] < pt[0] < rect[0]+rect[2] and rect[1] < pt[1] < rect[1]+rect[3]
-    return logic
+    return rect[0] < pt[0] < rect[0]+rect[2] and rect[1] < pt[1] < rect[1]+rect[3]
+
+
+'''
+Detect lines in image
+@Return list of lines
+
+Source:
+https://docs.opencv.org/3.0-beta/doc/py_tutorials/py_imgproc/py_houghlines/py_houghlines.html
+'''
+def detectLines(detect_img, output_img = None, color = [255, 255, 255]):
+    edges = cv2.Canny(detect_img,50,120)
+    minLineLength = 20
+    maxLineGap = 50
+    lines = cv2.HoughLinesP(edges,1,np.pi/180,100,minLineLength,maxLineGap)
+    i = len(lines)-1
+
+    if output_img is not None:
+        while i > 0:
+            i -= 1;
+            for x1,y1,x2,y2 in lines[i]:
+                cv2.line(output_img,(x1,y1),(x2,y2),color,2)
+
+    return lines, output_img
+
+if __name__ == "__main__":
+    main()
