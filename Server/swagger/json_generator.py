7372e77c5696d8f2c6e7f140ca52abe725f74553
Grebtsew <danwe980@student.liu.se>
2021-03-17 00:36:25 +0100

    Update json_generator.py

--- Server/swagger/json_generator.py
+++ Server/swagger/json_generator.py
@@ -3,3 +3,35 @@ Waits for RestAPI to start
 Then collects help functions
 And generates a new swagger.json file!
 """
+import requests
+
+
+url="http://localhost:8000"
+
+# Wait for api is up
+# Update fail response some times to let gui know we could not connect and so on...
+
+# send get help for post, put and get!
+json = {'func': 'help'}
+print("----- Sending help request to get ----- ")
+response = requests.get(url, params=json)
+print(response.text)
+print("----- Sending help request to post ----- ")
+response = requests.post(url, data=jsonlib.dumps(json))
+print(response.text)
+print("----- Sending help request to put ----- ")
+headers = {'Content-Type': 'html/text'}
+response = requests.put(url, headers=headers, data=b'not empty', params=json)
+print(response.text)
+
+# Get Templates
+
+
+# Create output for received objects!
+
+
+# Create final file
+
+# Move final file
+
+# return success or fail
