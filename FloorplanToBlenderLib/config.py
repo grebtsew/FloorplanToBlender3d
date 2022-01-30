d2f0b0b275813467df7f372270862d1154714924
Grebtsew <danwe980@student.liu.se>
2022-01-30 22:24:31 +0100

    Updated config and floorplan class

--- FloorplanToBlenderLib/config.py
+++ FloorplanToBlenderLib/config.py
@@ -2,7 +2,6 @@ import configparser
 import os
 import cv2
 
-from . import image
 from . import IO
 from . import const
 from . import calculate
@@ -18,12 +17,11 @@ Copyright (C) 2021 Daniel Westberg
 # TODO: add config security check, before start up!
 # TODO: safe read, use this func instead of repeating code everywhere!
 
-
-def read_calibration():
+def read_calibration(config_path):
     """
     Read all calibrations
     """
-    calibrations = get(const.WALL_CALIBRATION)
+    calibrations = get(config_path, const.WALL_CALIBRATION)
     if calibrations is None:
         calibrations = create_image_scale_calibration()
     elif calibrations[const.STR_CALIBRATION_IMAGE_PATH] == "":  # TODO: fix if deleted!
@@ -70,15 +68,23 @@ def generate_file():
     """
     Generate new config file, if no exist
     """
+    # create System Settings
     conf = configparser.ConfigParser()
-
-    conf[const.DEFAULT] = {
-        const.STR_IMAGE_PATH: const.DEFAULT_IMAGE_PATH,
-        const.STR_OUT_FORMAT: const.DEFAULT_OUT_FORMAT,
+    conf["SYSTEM"] = {
         const.STR_OVERWRITE_DATA: const.DEFAULT_OVERWRITE_DATA,  # TODO: implement!
         const.STR_BLENDER_INSTALL_PATH: IO.get_blender_os_path(),
-        #const.STR_FILE_STRUCTURE: const.DEFAULT_FILE_STRUCTURE,
+    }
+
+    with open(const.SYSTEM_CONFIG_FILE_NAME, "w") as configfile:
+        conf.write(configfile)
+
+    # create Default floorplan Settings
+    conf = configparser.ConfigParser()
+    conf["IMAGE"] = {
+        const.STR_IMAGE_PATH: const.DEFAULT_IMAGE_PATH,
+        const.STR_OUT_FORMAT: const.DEFAULT_OUT_FORMAT,
         const.STR_MODE: const.DEFAULT_MODE,
+        "COLOR": None,
     }
 
     conf[const.FEATURES] = {
@@ -99,9 +105,8 @@ def generate_file():
         const.STR_WALL_SIZE_CALIBRATION: const.DEFAULT_WALL_SIZE_CALIBRATION,
     }
 
-    with open(const.SYSTEM_CONFIG_FILE_NAME, "w") as configfile:
+    with open(const.IMAGE_DEFAULT_CONFIG_FILE_NAME, "w") as configfile:
         conf.write(configfile)
-
 
 def show(conf):
     """
@@ -111,13 +116,13 @@ def show(conf):
         print(key, conf[key])
 
 
-def update(label, values):
+def update(path, label, values):
     """
     Update a config category
     """
     conf = get_all()
     conf[label] = values
-    with open(const.SYSTEM_CONFIG_FILE_NAME, "w") as configfile:
+    with open(path, "w") as configfile:
         conf.write(configfile)
 
 
@@ -130,29 +135,29 @@ def file_exist(name):
     return os.path.isfile(name)
 
 
-def get_all(path=None):
+def get_all(path):
     """
     Read and return values
     @Return default values
     """
     config = configparser.ConfigParser()
 
-    if not file_exist(const.SYSTEM_CONFIG_FILE_NAME):
+    if not file_exist(path):
         generate_file()
-    config.read(const.SYSTEM_CONFIG_FILE_NAME)
+    config.read(path)
     return config
 
 
-def get(label):
+def get(config_path, label):
     """
     Read and return values
     @Return default values
     """
     conf = configparser.ConfigParser()
 
-    if not file_exist(const.SYSTEM_CONFIG_FILE_NAME):
+    if not file_exist(config_path):
         generate_file()
-    conf.read(const.SYSTEM_CONFIG_FILE_NAME)
+    conf.read(config_path)
     return conf[label]
 
 
@@ -167,8 +172,5 @@ def get_default():
         generate_file()
     config.read(const.SYSTEM_CONFIG_FILE_NAME)
     return (
-        config[const.DEFAULT][const.STR_IMAGE_PATH],
-        config[const.DEFAULT][const.STR_BLENDER_INSTALL_PATH],
-        config[const.DEFAULT][const.STR_FILE_STRUCTURE],
-        config[const.DEFAULT][const.STR_MODE],
+        config["SYSTEM"][const.STR_BLENDER_INSTALL_PATH],
     )
--- FloorplanToBlenderLib/floorplan.py
+++ FloorplanToBlenderLib/floorplan.py
@@ -1,3 +1,5 @@
+from . import const
+from . import config
 """
 Floorplan
 This file contains functions for handling the floorplan class.
@@ -10,16 +12,21 @@ def new_floorplan(config):
     """
     Creates and returns a new floorplan class from config file
     """
-    pass
+    return floorplan(config)
 
 class floorplan():
     """
     This class contains the representation of each floorplan
     The class is needed to simplify the code and it's costumizability
     """
-    
-    def __init__(self):
-        pass
+
+    def __init__(self, conf=None):
+        if conf is None:
+            # use default
+            conf = const.IMAGE_DEFAULT_CONFIG_FILE_NAME
+        
+        settings = config.get_all(conf)
+        locals().update(settings) # generate variables from settings
 
     def __str__(self):
         print(vars(self))
