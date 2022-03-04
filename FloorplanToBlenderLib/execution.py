fixed pivot bug and scaling

FloorplanToBlenderLib/execution.py
@@ -74,7 +74,7 @@ def AngleBtw2Points(pointA, pointB):
   return degrees(atan2(changeInY,changeInX)) 
 
 def multiple_cylinder(
-    floorplans, amount_per_level, radie, degree, world_direction=None, world_position=np.array([0,0,0]), world_rotation=np.array([0,0,0]), world_scale=np.array([1,1,1])
+    floorplans, amount_per_level, radie, degree, world_direction=None, world_position=np.array([0,0,0]), world_rotation=np.array([0,0,0]), world_scale=np.array([1,1,1], margin=np.array([0,0,0]))
 ):  
     """
     Generates several new apartments in a cylindric shape
@@ -87,6 +87,8 @@ def multiple_cylinder(
     @Param degree - how many degree should the circle be, 0-360
     @Return paths to image data
     """
+    # TODO: add world rotation solution
+    # TODO: add margin solution
     data_paths = list()
     curr_index = 0
     curr_level = 0

FloorplanToBlenderLib/generate.py
@@ -37,6 +37,8 @@ def generate_all_files(floorplan, info, world_direction = None,world_scale=np.ar
             transform.list_to_nparray(floorplan.position)+transform.list_to_nparray(world_position),
             " rot ",
             transform.list_to_nparray(floorplan.rotation)+transform.list_to_nparray(world_rotation),
+            " scale ",
+            scale,
             " -----",
         )
 
@@ -69,7 +71,7 @@ def generate_all_files(floorplan, info, world_direction = None,world_scale=np.ar
             Door(gray, path, floorplan.image_path, scale_factor, scale, info)
 
     generate_transform_file(
-        floorplan.image_path, path, info, floorplan.position, world_position, floorplan.rotation, world_rotation, shape, path, origin_path
+        floorplan.image_path, path, info, floorplan.position, world_position, floorplan.rotation, world_rotation, scale, shape, path, origin_path
     )
 
     if floorplan.position is not None:
@@ -93,8 +95,8 @@ def validate_shape(old_shape, new_shape):
 
 
 def generate_transform_file(
-    img_path, path, info, position, world_position, rotation, world_rotation, shape, data_path, origin_path
-):  
+    img_path, path, info, position, world_position, rotation, world_rotation, scale, shape, data_path, origin_path
+    ):  
     """
     Generate transform of file
     A transform contains information about an objects position, rotation.
@@ -111,6 +113,11 @@ def generate_transform_file(
         transform[const.STR_POSITION] = np.array([0, 0, 0])
     else:
         transform[const.STR_POSITION] = position+ world_position
+
+    if scale is None:
+        transform["scale"] = np.array([1,1,1])
+    else:
+        transform["scale"] = scale
 
     if rotation is None:
         transform[const.STR_ROTATION] = np.array([0, 0, 0])

FloorplanToBlenderLib/stacking.py
@@ -119,7 +119,7 @@ def ADD(
 
     if mode == "cylinder":        
         return  execution.multiple_cylinder(
-            floorplans, amount_per_level, radie, degree, world_direction=dir, world_position=transform.list_to_nparray(worldpositionoffset), world_rotation=transform.list_to_nparray(worldrotationoffset), world_scale=transform.list_to_nparray(worldscale)
+            floorplans, amount_per_level, radie, degree, world_direction=dir, world_position=transform.list_to_nparray(worldpositionoffset, np.array([0,0,0])), world_rotation=transform.list_to_nparray(worldrotationoffset, np.array([0,0,0])), world_scale=transform.list_to_nparray(worldscale), margin=transform.list_to_nparray(margin, np.array([0,0,0]))
         )
     else:
-        return execution.multiple_axis(floorplans, mode, dir, transform.list_to_nparray(margin), transform.list_to_nparray(worldpositionoffset), transform.list_to_nparray(worldrotationoffset), transform.list_to_nparray(worldscale))
+        return execution.multiple_axis(floorplans, mode, dir, transform.list_to_nparray(margin, np.array([0,0,0])), transform.list_to_nparray(worldpositionoffset, np.array([0,0,0])), transform.list_to_nparray(worldrotationoffset, np.array([0,0,0])), transform.list_to_nparray(worldscale))

FloorplanToBlenderLib/transform.py
@@ -134,16 +134,14 @@ def scale_point_to_vector(boxes, pixelscale=100, height=0, scale=np.array([1,1,1
     res = []
     for box in boxes:
         for pos in box:
-            res.extend([[(pos[0]*scale[0])/pixelscale, (pos[1]*scale[1])/pixelscale, height*scale[2]]])
+            res.extend([[(pos[0])/pixelscale, (pos[1])/pixelscale, height]])
     return res
 
-
-def list_to_nparray(list):
+def list_to_nparray(list, default=np.array([1,1,1])):
     if list is None:
-        return np.array([1,1,1])
+        return default
     else:
         return np.array([list[0],list[1],list[2]])
-
 
 def create_4xn_verts_and_faces(
     boxes, height=1, pixelscale=100, scale=np.array([1,1,1]), ground=False, ground_height=const.WALL_GROUND
@@ -211,10 +209,10 @@ def create_nx4_verts_and_faces(boxes, height=1, scale=np.array([1,1,1]), pixelsc
                 # link to first pos
 
             # Create all 3d poses for each wall
-            temp_verts.extend([((current[0] * scale[0])/pixelscale, (current[1] * scale[1])/pixelscale, ground)])
-            temp_verts.extend([((current[0] * scale[0])/pixelscale, (current[1] * scale[1])/pixelscale, (height-ground)*scale[2])])
-            temp_verts.extend([((next_vert[0] * scale[0])/pixelscale, (next_vert[1] * scale[1])/pixelscale, ground)])
-            temp_verts.extend([((next_vert[0] * scale[0])/pixelscale, (next_vert[1] * scale[1])/pixelscale, (height-ground)*scale[2])])
+            temp_verts.extend([((current[0] )/pixelscale, (current[1] )/pixelscale, ground)])
+            temp_verts.extend([((current[0] )/pixelscale, (current[1] )/pixelscale, (height-ground))])
+            temp_verts.extend([((next_vert[0] )/pixelscale, (next_vert[1] )/pixelscale, ground)])
+            temp_verts.extend([((next_vert[0] )/pixelscale, (next_vert[1] )/pixelscale, (height-ground))])
 
             # add wall verts to verts
             box_verts.extend([temp_verts])
@@ -247,8 +245,8 @@ def create_verts(boxes, height, pixelscale=100, scale=np.array([1,1,1])):
         for pos in box:
 
             # add and convert all positions
-            temp_verts.extend([((pos[0][0]*scale[0]) / pixelscale, (pos[0][1]*scale[1]) / pixelscale, 0.0)])
-            temp_verts.extend([((pos[0][0]*scale[0]) / pixelscale, (pos[0][1]*scale[1]) / pixelscale, height*scale[2])])
+            temp_verts.extend([((pos[0][0]) / pixelscale, (pos[0][1]) / pixelscale, 0.0)])
+            temp_verts.extend([((pos[0][0]) / pixelscale, (pos[0][1]) / pixelscale, height)])
 
         # add box to list
         verts.extend(temp_verts)